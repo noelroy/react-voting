dd6275c0a7ba60628a19691aa253ce15c6f02c39
Noel Roy <noelroy96@gmail.com>
2018-02-13 12:30:54 +0530

    React Router and firebase configured

---
+++ src/containers/App/firebase-config.js
@@ -0,0 +1,9 @@
+  export default  {
+    apiKey: "AIzaSyD4N-MN5eSQo7fxa9eIWSs2km7KjWwGqeI",
+    authDomain: "reddit-clone-5c859.firebaseapp.com",
+    databaseURL: "https://reddit-clone-5c859.firebaseio.com",
+    projectId: "reddit-clone-5c859",
+    storageBucket: "reddit-clone-5c859.appspot.com",
+    messagingSenderId: "845370364429"
+  };
+
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
+
+import config from './firebase-config';
 
 class App extends Component {
+  constructor() {
+    super();
+    firebase.initializeApp(config);
+  }
+  componentWillMount() {
+    let postsRef = firebase.database().ref('posts');
+
+    let _this = this;
+
+    postsRef.on("value", function(snapshot) {
+      console.log(snapshot.val());
+
+      _this.setState({
+        posts : snapshot.val(),
+        loading : false
+      });
+    });
+  }
   render() {
     return (
       <div className="App">
-      Hello
+      {this.props.children && React.cloneElement(this.props.children, {
+        firebaseRef : firebase.database.ref("posts"),
+        posts : this.state.posts,
+        loading : this.state.loading
+      })}
       </div>
     );
   }
